b0eb340a38e9d45d6653f0b9a0d686f9b458274d
Bianca Besciu <bianca.besciu@gmail.com>
2018-02-21 15:30:16 +0100

    cascadian progress bar

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,12 @@
 (function(document){
     //skill bar progress
     const _bars = [].slice.call(document.querySelectorAll('.progress-bar'));
-    _bars.map(function (bar) {
+    _bars.map(function (bar, index) {
+        //animate the progress bar
         setTimeout(function () {
             bar.style.width = bar.dataset.percent;
-        }, 0);
+        //    cascadian progress
+        }, index * 1000);
     })
 })(document);
 
