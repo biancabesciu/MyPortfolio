9a7adfa5b7d36cf5700d5e11817e8d04fdbd228c
Bianca Besciu <bianca.besciu@gmail.com>
2018-02-21 15:27:14 +0100

    making the progrress bar work with js

--- js/animation.js
+++ js/animation.js
@@ -1,1 +1,11 @@
-var sass = require('node-sass');
+(function(document){
+    //skill bar progress
+    const _bars = [].slice.call(document.querySelectorAll('.progress-bar'));
+    _bars.map(function (bar) {
+        setTimeout(function () {
+            bar.style.width = bar.dataset.percent;
+        }, 0);
+    })
+})(document);
+
+
