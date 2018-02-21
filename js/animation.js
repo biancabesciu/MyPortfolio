1c84c528ac07322f139042cd37dc9e4c12c37c77
Bianca Besciu <bianca.besciu@gmail.com>
2018-02-21 17:02:18 +0100

    navbar animation

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,72 @@
 (function(document){
     //skill bar progress
     const _bars = [].slice.call(document.querySelectorAll('.progress-bar'));
+
     _bars.map(function (bar, index) {
         //animate the progress bar
         setTimeout(function () {
             bar.style.width = bar.dataset.percent;
-        //    cascadian progress
+        //cascadian progress
         }, index * 1000);
-    })
+    });
+
+    //Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+         $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 57
+    });
+
+    // Collapse Navbar
+    const navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+    // Scroll reveal calls
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+    sr.reveal('.sr-button', {
+        duration: 1000,
+        delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
+
+    // Magnific popup calls
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+
 })(document);
 
 
